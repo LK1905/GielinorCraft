f666c4b62348abd48f56ce233c875205d622526e
LK1905 <liam1905kell@hotmail.com>
2019-12-30 15:32:04 +1100

    Adding setup files and other changes

---
+++ src/main/java/com/lk1905/gielinorcraft/Gielinorcraft.java
@@ -0,0 +1,9 @@
+package com.lk1905.gielinorcraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("gielinorcraft")
+public class Gielinorcraft {
+
+	public static final String MODID = "gielinorcraft";
+}
